373395dbd8caaf33687b3bd809762d5eda215741
Meghana Balarama <meghana1704@gmail.com>
2022-04-21 14:52:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,32 +11,33 @@ st.set_page_config(
 
 st.title("Video transcript Summarizer!")
 
-is_youtube_video = st.radio(
-            "Is this a YouTube video?",
-            ["Yes", "No"],
-            help="At present, you can choose to summarize either a YouTube video or a custom video that you upload",
-        )
+with st.form(key="my_form"):
+    is_youtube_video = st.radio(
+                "Is this a YouTube video?",
+                ["Yes", "No"],
+                help="At present, you can choose to summarize either a YouTube video or a custom video that you upload",
+            )
 
-if is_youtube_video=="Yes":
-  video = st.text_input("YouTube video URL")
-  submitted = st.form_submit_button(label="Summarize")
-  if submitted:
-      video_id = video.split("=")[1]
-      transcript = YouTubeTranscriptApi.get_transcript(video_id)
-      text = ""
-      for i in transcript:
-          text += ' ' + i['text']
+    if is_youtube_video=="Yes":
+      video = st.text_input("YouTube video URL")
+      submitted = st.form_submit_button(label="Summarize")
+      if submitted:
+          video_id = video.split("=")[1]
+          transcript = YouTubeTranscriptApi.get_transcript(video_id)
+          text = ""
+          for i in transcript:
+              text += ' ' + i['text']
 
-      summarizer = pipeline('summarization', model='facebook/bart-large-cnn', max_length=100)
-      num_iters = int(len(text)/1000)
-      summarized_text = []
-      for i in range(0, num_iters + 1):
-        start = 0
-        start = i * 1000
-        end = (i + 1) * 1000
-        out = summarizer(text[start:end])[0]['summary_text']
-        summarized_text.append(out)
+          summarizer = pipeline('summarization', model='facebook/bart-large-cnn', max_length=100)
+          num_iters = int(len(text)/1000)
+          summarized_text = []
+          for i in range(0, num_iters + 1):
+            start = 0
+            start = i * 1000
+            end = (i + 1) * 1000
+            out = summarizer(text[start:end])[0]['summary_text']
+            summarized_text.append(out)
 
-      st.write("SUMMARY")
-      st.write(str(summarized_text))
+          st.write("SUMMARY")
+          st.write(str(summarized_text))
 
