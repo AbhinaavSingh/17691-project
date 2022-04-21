a1391f4fc0aa0d6ebe31a6674460c20c0633c716
Meghana Balarama <meghana1704@gmail.com>
2022-04-21 14:46:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,24 @@ is_youtube_video = st.radio(
 
 if is_youtube_video=="Yes":
   video = st.text_input("YouTube video URL")
-  video_id = video.split("=")[1]
-  transcript = YouTubeTranscriptApi.get_transcript(video_id)
   submitted = st.form_submit_button(label="Summarize")
-  
-  text = ""
-  for i in transcript:
-      text += ' ' + i['text']
+  if submitted:
+      video_id = video.split("=")[1]
+      transcript = YouTubeTranscriptApi.get_transcript(video_id)
+      text = ""
+      for i in transcript:
+          text += ' ' + i['text']
 
+      summarizer = pipeline('summarization', model='facebook/bart-large-cnn', max_length=100)
+      num_iters = int(len(text)/1000)
+      summarized_text = []
+      for i in range(0, num_iters + 1):
+        start = 0
+        start = i * 1000
+        end = (i + 1) * 1000
+        out = summarizer(text[start:end])[0]['summary_text']
+        summarized_text.append(out)
 
-  summarizer = pipeline('summarization', model='facebook/bart-large-cnn', max_length=100)
-  num_iters = int(len(text)/1000)
-  summarized_text = []
-  for i in range(0, num_iters + 1):
-    start = 0
-    start = i * 1000
-    end = (i + 1) * 1000
-    out = summarizer(text[start:end])[0]['summary_text']
-    summarized_text.append(out)
-  
-  st.write("SUMMARY")
-  st.write(str(summarized_text))
+      st.write("SUMMARY")
+      st.write(str(summarized_text))
 
